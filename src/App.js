dd645b4658754ebc76c4d7e17cf842056059d779
Camila Constanza Villafañe <camilaconstanzavillafanie@gmail.com>
2021-03-05 00:49:02 -0300

    Entrega de la clase 3, navBar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { NavBar } from './components/navBar';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Camila Constanza Villafañe
-          Tienda Online
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+     <NavBar />   
+    <h2>Nuestras carpas Tipis</h2>
+
+  </div>)
+
+;
 }
 
 export default App;
---
+++ src/components/navBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const NavBar = () => {
+    return( 
+    <div>
+    <nav>        
+    <div class="nav-wrapper cyan lighten-5">
+    <a href="#" class="brand-logo light-blue darken-4 typeTextLogo">Sueños de Retoño</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down cyan lighten-5">      
+        <li><a href="#" class="colorTextMenu" >Home</a></li>
+        <li><a href="#" class="colorTextMenu">Tienda Online</a></li>
+        <li><a href="#" class="colorTextMenu">Armá tu pijamada</a></li>
+        <li><a href="#" class="colorTextMenu">Galeria</a></li>          
+        <li><a href="#" class="colorTextMenu">Contactanos</a></li>
+      </ul>
+    </div>
+  </nav>
+  </div>  
+    );
+}
